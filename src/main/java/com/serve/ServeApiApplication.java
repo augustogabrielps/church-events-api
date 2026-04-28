62d7f39eb3025f616169eff4145a668ed54abeb9
astos <deuvoid@gmail.com>
2026-04-27 21:47:42 -0300

    Initial commit

---
+++ src/main/java/com/serve/ServeApiApplication.java
@@ -0,0 +1,13 @@
+package com.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeApiApplication.class, args);
+	}
+
+}
